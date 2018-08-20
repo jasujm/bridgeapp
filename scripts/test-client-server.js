0ef177571b35c68b07fa4a4544893eeb00ee3b04
Jaakko Moisio <jaakko@moisio.fi>
2018-08-20 23:05:28 +0200

    Update test scripts to use new config mechanism

--- scripts/test-client-server.js
+++ scripts/test-client-server.js
@@ -5,7 +5,7 @@ const { spawn } = require("child_process");
 const secretKey = "JTKVSB%%)wK0E.X)V>+}o?pNmC{O&4W4b!Ni{Lh6";
 const publicKey = "rq:rM>}U?@Lns47E1%kR.o@n%FcmmsL/@{H8]yf7";
 
-const server = spawn("bridge", ["--config", "-", "tcp://127.0.0.1:5555"]);
+const server = spawn("bridge", ["--config", "-"]);
 server.stdin.write(`curve_secret_key = "${secretKey}"\n`);
 server.stdin.end(`curve_public_key = "${publicKey}"\n`);
 
--- scripts/test-peers.js
+++ scripts/test-peers.js
@@ -2,17 +2,48 @@ const uuid = require("uuid/v4");
 const client = require("../bridge/client");
 const { spawn } = require("child_process");
 
-spawn("bridgecs", ["tcp://127.0.0.1:5650", "tcp://127.0.0.1:5750"]);
-spawn("bridgecs", ["tcp://127.0.0.1:5660", "tcp://127.0.0.1:5760"]);
-spawn("bridgecs", ["tcp://127.0.0.1:5670", "tcp://127.0.0.1:5770"]);
-spawn("bridgecs", ["tcp://127.0.0.1:5680", "tcp://127.0.0.1:5780"]);
+const secretKey = "JTKVSB%%)wK0E.X)V>+}o?pNmC{O&4W4b!Ni{Lh6";
+const publicKey = "rq:rM>}U?@Lns47E1%kR.o@n%FcmmsL/@{H8]yf7";
+const gameUuid = uuid();
 
-spawn("bridge", ['--positions', '["north"]', '--connect', '["tcp://localhost:5560","tcp://localhost:5570","tcp://localhost:5580"]', '--cs-cntl', 'tcp://127.0.0.1:5650', '--cs-peer', 'tcp://127.0.0.1:5750', 'tcp://127.0.0.1:5550']);
-spawn("bridge", ['--positions', '["east"]', '--connect', '["tcp://localhost:5550","tcp://localhost:5570","tcp://localhost:5580"]', '--cs-cntl', 'tcp://127.0.0.1:5660', '--cs-peer', 'tcp://127.0.0.1:5760', 'tcp://127.0.0.1:5560']);
-spawn("bridge", ['--positions', '["south"]', '--connect', '["tcp://localhost:5550","tcp://localhost:5560","tcp://localhost:5580"]', '--cs-cntl', 'tcp://127.0.0.1:5670', '--cs-peer', 'tcp://127.0.0.1:5770', 'tcp://127.0.0.1:5570']);
-spawn("bridge", ['--positions', '["west"]', '--connect', '["tcp://localhost:5550","tcp://localhost:5560","tcp://localhost:5570"]', '--cs-cntl', 'tcp://127.0.0.1:5680', '--cs-peer', 'tcp://127.0.0.1:5780', 'tcp://127.0.0.1:5580']);
+function spawnBridge(positions, peers, cscntl, cspeer, port) {
+    const server = spawn("bridge", ['--config', '-']);
+    config = `bind_base_port = ${port}
+curve_secret_key = "${secretKey}"
+curve_public_key = "${publicKey}"
+game {
+  uuid = "${gameUuid}",
+  positions_controlled = { ${positions.map((position) => `"${position}"`).join()} },
+  peers = {
+    ${peers.map((peer) => `{endpoint = "${peer}", server_key = "${publicKey}"}`).join()}
+  },
+  card_server = {
+    control_endpoint = "${cscntl}",
+    base_peer_endpoint = "${cspeer}",
+  },
+}
+`;
+    server.stdin.end(config);
+}
 
-client("tcp://127.0.0.1:5550").catch((err) => console.log("failed with error:", err));
-client("tcp://127.0.0.1:5560").catch((err) => console.log("failed with error:", err));
-client("tcp://127.0.0.1:5570").catch((err) => console.log("failed with error:", err));
-client("tcp://127.0.0.1:5580").catch((err) => console.log("failed with error:", err));
+function spawnCardServer(cntl, peer) {
+    const server = spawn(
+        "bridgecs",
+        ['--secret-key-file', '-', '--public-key-file', '-', cntl, peer]);
+    server.stdin.end(`${secretKey} ${publicKey}\n`);
+}
+
+spawnCardServer("tcp://127.0.0.1:5650", "tcp://127.0.0.1:5750");
+spawnCardServer("tcp://127.0.0.1:5660", "tcp://127.0.0.1:5760");
+spawnCardServer("tcp://127.0.0.1:5670", "tcp://127.0.0.1:5770");
+spawnCardServer("tcp://127.0.0.1:5680", "tcp://127.0.0.1:5780");
+
+spawnBridge(["north"], ["tcp://localhost:5560","tcp://localhost:5570","tcp://localhost:5580"], 'tcp://127.0.0.1:5650', 'tcp://127.0.0.1:5750', 5550);
+spawnBridge(["east"], ["tcp://localhost:5550","tcp://localhost:5570","tcp://localhost:5580"], 'tcp://127.0.0.1:5660', 'tcp://127.0.0.1:5760', 5560);
+spawnBridge(["south"], ["tcp://localhost:5550","tcp://localhost:5560","tcp://localhost:5580"], 'tcp://127.0.0.1:5670', 'tcp://127.0.0.1:5770', 5570);
+spawnBridge(["west"], ["tcp://localhost:5550","tcp://localhost:5560","tcp://localhost:5570"], 'tcp://127.0.0.1:5680', 'tcp://127.0.0.1:5780', 5580);
+
+client("tcp://127.0.0.1:5550", publicKey).catch((err) => console.log("failed with error:", err));
+client("tcp://127.0.0.1:5560", publicKey).catch((err) => console.log("failed with error:", err));
+client("tcp://127.0.0.1:5570", publicKey).catch((err) => console.log("failed with error:", err));
+client("tcp://127.0.0.1:5580", publicKey).catch((err) => console.log("failed with error:", err));
