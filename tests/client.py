e4683a4b12d2aad6884b7285cac9826e21a9db30
Jaakko Moisio <jaakko@moisio.fi>
2020-06-16 23:52:22 +0200

    Make test client more robust against missing subscription

--- tests/client.py
+++ tests/client.py
@@ -14,28 +14,46 @@ from bridgeapp import bridgeprotocol, models
 click_log.basic_config()
 
 
+async def _get_turn_from_deal_state(client, game, player):
+    deal_state = await client.get_deal(game=game, player=player)
+    return deal_state.positionInTurn
+
+
+async def _get_turn_from_events(event_receiver):
+    async for event in event_receiver.events():
+        if event.type == "turn":
+            return models.Position(event.position)
+
+
 async def _play_bridge_game(
     client: bridgeprotocol.BridgeClient,
     event_receiver: bridgeprotocol.BridgeEventReceiver,
 ):
     player_uuids = {position: uuid.uuid4() for position in models.Position}
+    player_uuid = player_uuids[models.Position.north]
     game_uuid = await client.game()
     for position, player_uuid in player_uuids.items():
         await client.join(game=game_uuid, player=player_uuid, position=position)
-    async for event in event_receiver.events():
-        if event.type == "turn":
-            position_in_turn = models.Position(event.position)
-            player_uuid = player_uuids[position_in_turn]
-            player_state = await client.get_player(game=game_uuid, player=player_uuid)
-            with contextlib.suppress(bridgeprotocol.CommandFailure):
-                if calls := player_state.allowedCalls:
-                    await client.call(
-                        game=game_uuid, player=player_uuid, call=random.choice(calls)
-                    )
-                elif cards := player_state.allowedCards:
-                    await client.play(
-                        game=game_uuid, player=player_uuid, card=random.choice(cards)
-                    )
+    position_in_turn = await _get_turn_from_deal_state(client, game_uuid, player_uuid)
+    while True:
+        player_uuid = player_uuids[position_in_turn]
+        player_state = await client.get_player(game=game_uuid, player=player_uuid)
+        if calls := player_state.allowedCalls:
+            await client.call(
+                game=game_uuid, player=player_uuid, call=random.choice(calls)
+            )
+        elif cards := player_state.allowedCards:
+            await client.play(
+                game=game_uuid, player=player_uuid, card=random.choice(cards)
+            )
+        try:
+            position_in_turn = await asyncio.wait_for(
+                _get_turn_from_events(event_receiver), timeout=0.1
+            )
+        except asyncio.TimeoutError:
+            position_in_turn = await _get_turn_from_deal_state(
+                client, game_uuid, player_uuid
+            )
 
 
 async def _async_main(control_endpoint, event_endpoint):
