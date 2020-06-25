8c7d6e97dce278faa2a8cb7fcbde0326e295d1be
Jaakko Moisio <jaakko@moisio.fi>
2020-06-25 09:55:11 +0200

    [bridgeapp/web/api] Document PLAYER_UUID_NS

--- bridgeapp/web/api/utils.py
+++ bridgeapp/web/api/utils.py
@@ -7,9 +7,10 @@ import uuid
 
 from bridgeapp import settings
 
-player_uuid_ns = uuid.uuid5(settings.get_settings().uuid_namespace, "players")
+PLAYER_UUID_NS = uuid.uuid5(settings.get_settings().uuid_namespace, "players")
+"""UUID namespace for bridge players"""
 
 
 def generate_player_uuid(username: str) -> uuid.UUID:
     """Generate UUID for a player from ``username``"""
-    return uuid.uuid5(player_uuid_ns, username)
+    return uuid.uuid5(PLAYER_UUID_NS, username)
