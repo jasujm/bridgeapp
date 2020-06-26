34507942d00223f7e1ed8b8e6ea0aea79b88b7cf
Jaakko Moisio <jaakko@moisio.fi>
2020-06-26 09:59:27 +0200

    [tests/test_api] Fixturize dependencies

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,11 +14,13 @@ from bridgeapp import app, api, models
 
 @pytest.fixture
 def client():
+    """Yield mock client that interacts with the API"""
     return fastapi.testclient.TestClient(app)
 
 
 @pytest.fixture
 def mock_bridge_client(monkeypatch):
+    """Yield mock client that will be used by the API to interact with the bridge backend"""
     mock = unittest.mock.Mock(
         game=unittest.mock.AsyncMock(),
         join=unittest.mock.AsyncMock(),
@@ -28,23 +30,38 @@ def mock_bridge_client(monkeypatch):
     return mock
 
 
+@pytest.fixture
+def game_uuid():
+    """Yield an UUID for game"""
+    return uuid.uuid4()
+
+
+@pytest.fixture(params=["username", "anotheruser"])
+def username(request):
+    """Yield an username"""
+    return request.param
+
+
+@pytest.fixture
+def username_and_player_uuid(username):
+    """Yield a pair containing an username and corresponding player UUID"""
+    return username, api.utils.generate_player_uuid(username)
+
+
 def test_generate_player_uuid():
     assert isinstance(api.utils.generate_player_uuid("username"), uuid.UUID)
 
 
-def test_create_game(client, mock_bridge_client):
-    game_uuid = uuid.uuid4()
+def test_create_game(client, mock_bridge_client, game_uuid, username):
     mock_bridge_client.game.return_value = game_uuid
-    res = client.post("/api/v1/games", auth=("username", "secret"))
+    res = client.post("/api/v1/games", auth=(username, "secret"))
     assert res.status_code == fastapi.status.HTTP_201_CREATED
     assert res.headers["Location"] == f"/api/v1/games/{game_uuid}"
     assert res.json() == {"uuid": str(game_uuid)}
 
 
-@pytest.mark.parametrize("username", ["username", "anotheruser"])
-def test_read_game(client, mock_bridge_client, username):
-    game_uuid = uuid.uuid4()
-    player_uuid = api.utils.generate_player_uuid(username)
+def test_read_game(client, mock_bridge_client, game_uuid, username_and_player_uuid):
+    username, player_uuid = username_and_player_uuid
     deal_state = models.DealState(positionInTurn=models.Position.north)
     mock_bridge_client.get_deal.return_value = deal_state
     res = client.get(f"/api/v1/games/{game_uuid}", auth=(username, "secret"))
@@ -54,10 +71,8 @@ def test_read_game(client, mock_bridge_client, username):
     )
 
 
-@pytest.mark.parametrize("username", ["username", "anotheruser"])
-def test_add_player(client, mock_bridge_client, username):
-    game_uuid = uuid.uuid4()
-    player_uuid = api.utils.generate_player_uuid(username)
+def test_add_player(client, mock_bridge_client, game_uuid, username_and_player_uuid):
+    username, player_uuid = username_and_player_uuid
     mock_bridge_client.join.return_value = None
     res = client.post(f"/api/v1/games/{game_uuid}/players", auth=(username, "secret"))
     assert res.status_code == fastapi.status.HTTP_204_NO_CONTENT
