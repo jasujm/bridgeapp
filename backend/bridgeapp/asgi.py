17e3df57c0ea3211a77cae5c156cec68ea10d664
Jaakko Moisio <jaakko@moisio.fi>
2020-07-01 22:54:07 +0200

    [backend] Add CORS middleware for development

--- backend/bridgeapp/asgi.py
+++ backend/bridgeapp/asgi.py
@@ -4,9 +4,25 @@ Entry point to the ASGI app
 """
 
 import fastapi
+import fastapi.middleware.cors
 
 from . import api, settings
 
 app = fastapi.FastAPI()
+
+# Allow cross-origin references for development
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    fastapi.middleware.cors.CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api.router, prefix=settings.get_settings().api_v1_prefix)
