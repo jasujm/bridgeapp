9f1645943a5835a33efc2b810d8c706ced5a27df
Jaakko Moisio <jaakko@moisio.fi>
2020-11-18 22:35:13 +0100

    Configure data directory

--- config/bridge/config.lua
+++ config/bridge/config.lua
@@ -1,5 +1,14 @@
 bind_address = "*"
 bind_base_port = 5555
 
-curve_public_key = os.getenv("bridge_curve_public_key")
-curve_secret_key = os.getenv("bridge_curve_secret_key")
+curve_public_key = os.getenv("bridge_curve_publickey")
+curve_secret_key = os.getenv("bridge_curve_secretkey")
+
+bridgeapp_curve_publickey = os.getenv("bridge_bridgeapp_curve_publickey")
+if bridgeapp_public_key then
+   known_nodes = {
+      { public_key = bridgeapp_curve_publickey, user_id = "bridgeapp" },
+   }
+end
+
+data_dir = os.getenv("bridge_data_dir")
