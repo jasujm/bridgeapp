ad9614d69f8a98bdc471c40fc08506a9404a752c
Jaakko Moisio <jaakko@moisio.fi>
2020-11-18 22:57:03 +0100

    [backend] Split curve settings into multiple environment variables

--- backend/bridgeapp/settings.py
+++ backend/bridgeapp/settings.py
@@ -11,7 +11,7 @@ import functools
 import typing
 import uuid
 
-from pydantic import BaseSettings, Field
+from pydantic import BaseSettings, Field, root_validator
 
 from .bridgeprotocol.utils import TCP_ENDPOINT_RE
 from .bridgeprotocol import CurveKeys
@@ -34,6 +34,30 @@ class Settings(BaseSettings):
         regex=TCP_ENDPOINT_RE.pattern,
     )
 
+    curve_serverkey: typing.Optional[str] = Field(
+        title="Bridge backend server key",
+        description="""
+The public key of the bridge backend server. If this is set together with the
+client keys, use ZeroMQ CURVE mechanism to connect securely with the bridge
+backend."""
+    )
+
+    curve_publickey: typing.Optional[str] = Field(
+        title="Bridge client public key",
+        description="""
+The public key of the bridge client. If this is set together with the client
+keys, use ZeroMQ CURVE mechanism to connect securely with the bridge backend.
+"""
+    )
+
+    curve_secretkey: typing.Optional[str] = Field(
+        title="Bridge client secret key",
+        description="""
+The secret key of the bridge client. If this is set together with the client
+keys, use ZeroMQ CURVE mechanism to connect securely with the bridge backend.
+"""
+    )
+
     uuid_namespace: uuid.UUID = Field(
         default_factory=uuid.uuid4,
         title="Root namespace for UUIDs",
@@ -43,9 +67,21 @@ on their username. This setting is used as the root namespace. It should be set
 to a known value to ensure consistent UUID generation across runs.""",
     )
 
-    curve_keys: typing.Optional[CurveKeys] = Field(
-        None, title="CURVE ZeroMQ keys to establish connection with the backend"
-    )
+    @property
+    def curve_keys(self) -> CurveKeys:
+        """Return curve keys from the settings"""
+        return CurveKeys(
+            serverkey=self.curve_serverkey,
+            publickey=self.curve_publickey,
+            secretkey=self.curve_secretkey,
+        )
+
+    @root_validator
+    def _check_curve_keys(cls, values):
+        keys = [values.get(k) for k in ("curve_serverkey", "curve_publickey", "curve_secretkey")]
+        if not all(key is None for key in keys) and not all(key is not None for key in keys):
+            raise ValueError("Either all or none of the curve keys must be defined")
+        return values
 
     class Config:  # pylint: disable=missing-class-docstring
         env_prefix = "bridgeapp_"
