0ac481bfcd1b9b57a8bb93a2855e2f573533c6de
Jaakko Moisio <jaakko@moisio.fi>
2021-04-18 21:45:39 +0300

    [nginx] Add script to bootstrap TLS certificates
    The old inotify based flow was broken -- ditch that and instead run some
    commands in the containers manually.

---
+++ scripts/get-certs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+server_name=$1
+if [ ! $server_name ]; then
+    echo "Usage: get-certs.sh [server-name]"
+fi
+
+docker-compose exec certbot certbot certonly --webroot -w /var/www/certbot -d $1
+docker-compose up -d --force-recreate nginx
