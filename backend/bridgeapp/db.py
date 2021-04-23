574df04aa8a4afa1561cba3cb27b384284179b1c
Jaakko Moisio <jaakko@moisio.fi>
2021-04-23 21:46:58 +0300

    [backend] Delete commented-out code

--- backend/bridgeapp/db.py
+++ backend/bridgeapp/db.py
@@ -9,8 +9,6 @@ import sqlalchemy_utils.types as sqlt
 
 from .settings import settings
 
-# from .bridgeprotocol import models
-
 
 def _get_database_url():
     # TODO: Using aiopg since the default (asyncpg) doesn't work with
@@ -46,28 +44,6 @@ games = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlt.uuid.UUIDType, primary_key=True),
     sqlalchemy.Column("name", sqlalchemy.String(63), nullable=False, index=True),
 )
-
-"""
-Not sure if needed
-
-game_players = sqlalchemy.Table(
-    "game_players",
-    meta,
-    sqlalchemy.Column(
-        "game_id",
-        sqlt.uuid.UUIDType,
-        sqlalchemy.ForeignKey(games.c.id),
-        primary_key=True,
-    ),
-    sqlalchemy.Column("position", sqlalchemy.Enum(models.Position), primary_key=True),
-    sqlalchemy.Column(
-        "player_id",
-        sqlt.uuid.UUIDType,
-        sqlalchemy.ForeignKey(players.c.id),
-        nullable=False,
-    ),
-)
-"""
 
 meta.create_all(engine)
 
